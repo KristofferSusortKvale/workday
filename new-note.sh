d475ba9da78c7b2060a68cea82ba7b7072d12bbd
Kristoffer S Kvale <kristoffersusortkvale@gmail.com>
2022-08-20 04:18:52 +0200

    Added time to notes

--- new-note.sh
+++ new-note.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+# Getting current time
+time=`date +"%H:%M"`
+
 if [ "$1" = "good" ];
-then echo "$2" >> "$HOME/.workday/good.txt"
+then echo "$time $2" >> "$HOME/.workday/good.txt"
 
 elif [ "$1" = "bad" ];
-then echo "$2" >> "$HOME/.workday/bad.txt"
+then echo "$time $2" >> "$HOME/.workday/bad.txt"
 
 elif [ "$1" = "idea" ];
-then echo "$2" >> "$HOME/.workday/idea.txt"
+then echo "$time $2" >> "$HOME/.workday/idea.txt"
 
 elif [ "$1" = "problem" ];
-then echo "$2" >> "$HOME/.workday/problem.txt"
+then echo "$time $2" >> "$HOME/.workday/problem.txt"
 
 else echo "ERROR: Type not recognized"
 
